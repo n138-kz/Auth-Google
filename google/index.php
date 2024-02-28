397c4d2c40188a38a96a4c35ab00f24726b20984
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-02-29 04:31:06 +0900

    Access-Control-Allow-Methods +PATCH

--- google/index.php
+++ google/index.php
@@ -6,8 +6,8 @@
 header('Content-Type: text/plain');
 header('Content-Type: Application/json');
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT, DELETE");
 
 $config = dirname(__FILE__) . '/' . '.env';
 if ( file_exists($config) && filesize($config) > 0 ) {

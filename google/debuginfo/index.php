77e008c37afd8f8df4b7aa63ac0613794bba6c67
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-03-02 08:41:15 +0900

    Content-Type

--- google/debuginfo/index.php
+++ google/debuginfo/index.php
@@ -5,7 +5,7 @@
 ]);
 date_default_timezone_set('Asia/Tokyo');
 header('Content-Type: text/plain');
-header('Content-Type: Application/json');
+header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");

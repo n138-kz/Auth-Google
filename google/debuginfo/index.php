ee375243650acfea811a125e4cbc7644b588629e
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-03-02 08:53:35 +0900

    Content-Type: text/html; charset=UTF-8

--- google/debuginfo/index.php
+++ google/debuginfo/index.php
@@ -4,11 +4,7 @@
 	'name' 	          => 'AUTHNKEY',
 ]);
 date_default_timezone_set('Asia/Tokyo');
-header('Content-Type: text/plain');
-header('Content-Type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding, Origin, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Content-Type: text/html; charset=UTF-8');
 
 $config = dirname(__FILE__) . '/../' . '.env';
 $config_loaded = false;

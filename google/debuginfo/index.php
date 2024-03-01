ed73b561ca261ece3f552291c7ddfce5cb0b74d3
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-03-02 08:56:37 +0900

    認証サーバではないので認証系削除

--- google/debuginfo/index.php
+++ google/debuginfo/index.php
@@ -68,60 +68,15 @@ try {
 		exit(1);
 	}
 
-	if ( ( $result['google']['session']['iat'] == 0 ) || ( $result['google']['session']['exp'] == 0 ) ) {
-		/* token has expired */
-		$payload = false;
-
-		set_http_response_code(401);
-		$result['issue_at'] = microtime(TRUE);
-		$result['last_checkpoint'] = __LINE__;
-
-		echo json_encode( $result );
-		exit(1);
-	}
-
-	$result['authn'] = [
-		'sessions' => [
-			'id' => session_id(),
-			'name' => session_name(),
-		],
-	];
-	
 	set_http_response_code(200);
 	$result['issue_at'] = microtime(TRUE);
 	$result['last_checkpoint'] = __LINE__;
 
-	$_SESSION = [ 'authn' => $result ];
-
-	echo json_encode( $result );
 	exit(0);
 } catch (\Exception $th) {
 	set_http_response_code(500);
 	$result['issue_at'] = microtime(TRUE);
 	$result['last_checkpoint'] = __LINE__;
-
-	$th_json = [
-		'code' => $th->getCode(),
-		'message' => $th->getMessage(),
-		'file' => $th->getFile(),
-		'line' => $th->getLine(),
-		'trace' => $th->getTrace(),
-	];
-
-	$encrypt = [
-		'method' => 'AES-256-CBC',
-		'iv_length' => 0,
-		'iv' => null,
-		'option' => 0,
-		'encrypted' => null,
-		'decrypted' => null,
-	];
-	$encrypt['iv_length'] = openssl_cipher_iv_length($encrypt['method']);
-	$encrypt['iv'] = openssl_random_pseudo_bytes($encrypt['iv_length']);
-	$encrypt['option'] = 0;
-	$encrypt['encrypted'] = openssl_encrypt( json_encode($th_json), $encrypt['method'], 'passw0rd', $encrypt['option'], $encrypt['iv'] );
-	$result['exception_text'] = json_encode($encrypt);
-	$encrypt['decrypted'] = openssl_decrypt( $encrypt['encrypted'], $encrypt['method'], 'passw0rd', $encrypt['option'], $encrypt['iv'] );
 
 	error_log($th->getTraceAsString());
 
